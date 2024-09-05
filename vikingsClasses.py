9f3533238222515f65b448a2510ec81b1b98aca3
ankitask <ankitakalangutkar90@gmail.com>
2024-09-06 00:41:59 +0530

    soldier class update

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -6,78 +6,85 @@ import random
 class Soldier:
     def __init__(self, health, strength):
         # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
         # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
     
 
-# Viking
+# # Viking
 
-class Viking(Soldier):
-    def __init__(self, name, health, strength):
-        # your code here
+# class Viking(Soldier):
+#     def __init__(self, name, health, strength):
+#         # your code here
+#         self.name = name
+#         self.health = health
+#         self.strength = strength
 
-    def battleCry(self):
-        # your code here
+#     def battleCry(self):
+#         # your code here
 
-    def receiveDamage(self, damage):
-        # your code here
+#     def receiveDamage(self, damage):
+#         # your code here
 
-# Saxon
+# # Saxon
 
-class Saxon(Soldier):
-    def __init__(self, health, strength):
-        # your code here
+# class Saxon(Soldier):
+#     def __init__(self, health, strength):
+#         # your code here
 
-    def receiveDamage(self, damage):
-        # your code here
+#     def receiveDamage(self, damage):
+#         # your code here
 
-# Davicente
+# # Davicente
 
-class War():
-    def __init__(self):
-        # your code here
+# class War():
+#     def __init__(self):
+#         # your code here
 
-    def addViking(self, viking):
-        # your code here
+#     def addViking(self, viking):
+#         # your code here
     
-    def addSaxon(self, saxon):
-        # your code here
+#     def addSaxon(self, saxon):
+#         # your code here
     
-    def vikingAttack(self):
-        # your code here
+#     def vikingAttack(self):
+#         # your code here
     
-    def saxonAttack(self):
-        # your code here
+#     def saxonAttack(self):
+#         # your code here
 
-    def showStatus(self):
-        # your code here
-    pass
+#     def showStatus(self):
+#         # your code here
+#     pass
 
-#yop
-class War2:
+# #yop
+# class War2:
 
-    def __init__(self):
-        # your code here
+#     def __init__(self):
+#         # your code here
 
-    def addViking(self, Viking):
-        # your code here
+#     def addViking(self, Viking):
+#         # your code here
     
-    def addSaxon(self, Saxon):
-        # your code here
+#     def addSaxon(self, Saxon):
+#         # your code here
     
-    def vikingAttack(self):
-        # your code here
+#     def vikingAttack(self):
+#         # your code here
 
-    def saxonAttack(self):
-        # your code here
+#     def saxonAttack(self):
+#         # your code here
 
-    def showStatus(self):
-        # your code here
+#     def showStatus(self):
+#         # your code here
 
-    pass
+#     pass
 
 
