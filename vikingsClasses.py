b5226128d6d054f81e8d830f2593ad0a8a0947a3
Adeteju Enunwa <adetejut4real@gmail.com>
2024-09-07 11:46:51 +0200

    war  submission

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,79 +5,94 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
-    
+        self.health = health
+        self.strength = strength
     def attack(self):
-        # your code here
+         return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
+         self.health -= damage
     
 
-# Viking
+# Viking - this inherits from the soldier and has a name parameter,. Also has a battleCry() method and receiveDamage() method that provies custom input
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
+        pass
 
     def battleCry(self):
         # your code here
+        pass
 
     def receiveDamage(self, damage):
         # your code here
+        pass
 
-# Saxon
+# Saxon - inherits from the soldier , has no name parameter nut also a receiveDamage() methood that outpouts a custom message
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
         # your code here
+        pass
 
     def receiveDamage(self, damage):
         # your code here
+        pass
 
 # Davicente
 
 class War():
     def __init__(self):
         # your code here
+        pass
 
     def addViking(self, viking):
         # your code here
+        pass
     
     def addSaxon(self, saxon):
         # your code here
+        pass
     
     def vikingAttack(self):
         # your code here
+        pass
     
     def saxonAttack(self):
         # your code here
+        pass
 
     def showStatus(self):
         # your code here
-    pass
+        pass
 
 #yop
 class War2:
 
     def __init__(self):
         # your code here
+        pass
 
     def addViking(self, Viking):
         # your code here
+        pass
     
     def addSaxon(self, Saxon):
         # your code here
+        pass
     
     def vikingAttack(self):
         # your code here
+        pass
 
     def saxonAttack(self):
         # your code here
+        pass
 
     def showStatus(self):
         # your code here
-
-    pass
+        pass
 
 
