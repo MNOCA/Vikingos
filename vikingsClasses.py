bde5ddc9045816fe2bc7199b480475140fa74b54
ankitask <ankitakalangutkar90@gmail.com>
2024-09-07 17:09:12 +0530

    war update

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -55,27 +55,54 @@ class Saxon(Soldier):
         else:
             return f"A Saxon has died in combat"
 
-# # Davicente
+# Davicente
 
-# class War():
-#     def __init__(self):
-#         # your code here
+class War():
+    def __init__(self):
+        # your code here
+        self.vikingArmy = []
+        self.saxonArmy = []
 
-#     def addViking(self, viking):
-#         # your code here
+    def addViking(self, viking):
+        # your code here
+        self.vikingArmy.append(viking)
     
-#     def addSaxon(self, saxon):
-#         # your code here
+    def addSaxon(self, saxon):
+        # your code here
+        self.saxonArmy.append(saxon)
     
-#     def vikingAttack(self):
-#         # your code here
-    
-#     def saxonAttack(self):
-#         # your code here
+    def vikingAttack(self):
+        # your code here
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
 
-#     def showStatus(self):
-#         # your code here
-#     pass
+        result = saxon.receiveDamage(viking.strength)
+
+        if saxon.health <= 0:
+            self.saxonArmy.remove(saxon)
+
+        return result
+    
+    def saxonAttack(self):
+        # your code here
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+
+        result = viking.receiveDamage(saxon.strength)
+
+        if viking.health <= 0:
+            self.vikingArmy.remove(viking)
+            
+        return result
+
+    def showStatus(self):
+        # your code here
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy) == 1 and len(self.vikingArmy) == 1:
+            return "Vikings and Saxons are still in the thick of battle."
 
 # #yop
 # class War2:
