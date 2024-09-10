86c35f683df9ff6ee867713d4ea7a3402f325973
Alexandre D-Julin <53292656+adjulin@users.noreply.github.com>
2024-09-10 18:29:26 +0200

    Implemented classes to pass all tests

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,79 +5,159 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
-    
+        self.health = health
+        self.strength = strength
+
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
-    
+        self.health -= damage
+
 
 # Viking
 
 class Viking(Soldier):
+
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
 
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+
+        return f"{self.name} has died in act of combat"
+
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+
+        return "A Saxon has died in combat"
+
+
 
 # Davicente
 
 class War():
+
     def __init__(self):
-        # your code here
+
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def __repr__(self):
+        
+        string = f'\n{len(self.vikingArmy)} VIKINGS:\n'
+        for viking in self.vikingArmy:
+            string += f"{viking.name}, {viking.health}, {viking.strength}\n"
+        
+        string += f"{len(self.saxonArmy)} SAXONS\n"
+        for saxon in self.saxonArmy:
+            string += f"{saxon.health}, {saxon.strength}\n"
+        
+        return string
 
     def addViking(self, viking):
-        # your code here
-    
+        self.vikingArmy.append(viking)
+
     def addSaxon(self, saxon):
-        # your code here
-    
+        self.saxonArmy.append(saxon)
+
     def vikingAttack(self):
-        # your code here
-    
+
+        if len(self.vikingArmy) <= 0 or len(self.saxonArmy) <= 0:
+            return
+
+        # pick a random viking and saxon
+        viking = random.choice(self.vikingArmy)
+        saxon = random.choice(self.saxonArmy)
+
+        # apply damage
+        attack_result = saxon.receiveDamage(viking.attack())
+
+        # remove dead saxons from the list
+        self.saxonArmy = [saxon for saxon in self.saxonArmy if saxon.health > 0]
+
+        return attack_result
+
     def saxonAttack(self):
-        # your code here
+
+        if len(self.vikingArmy) <= 0 or len(self.saxonArmy) <= 0:
+            return
+
+        # pick a random viking and saxon
+        viking = random.choice(self.vikingArmy)
+        saxon = random.choice(self.saxonArmy)
+
+        # apply damage
+        attack_result = viking.receiveDamage(saxon.attack())
+
+        # remove dead viking from the list
+        self.vikingArmy = [viking for viking in self.vikingArmy if viking.health > 0]
+
+        return attack_result
 
     def showStatus(self):
-        # your code here
-    pass
+
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+
+        return "Vikings and Saxons are still in the thick of battle."
 
 #yop
-class War2:
+# class War2:
 
-    def __init__(self):
-        # your code here
+#     def __init__(self):
+#         # your code here
 
-    def addViking(self, Viking):
-        # your code here
+#     def addViking(self, Viking):
+#         # your code here
     
-    def addSaxon(self, Saxon):
-        # your code here
+#     def addSaxon(self, Saxon):
+#         # your code here
     
-    def vikingAttack(self):
-        # your code here
+#     def vikingAttack(self):
+#         # your code here
 
-    def saxonAttack(self):
-        # your code here
+#     def saxonAttack(self):
+#         # your code here
 
-    def showStatus(self):
-        # your code here
+#     def showStatus(self):
+#         # your code here
 
-    pass
+#     pass
 
 
+if __name__ == "__main__":
+
+    def generateViking():
+        return Viking("Harald", 300, 150)
+
+    def generateSaxon():
+        return Saxon(60, 25)
+
+    viking = generateViking()
+    saxon = generateSaxon()
+    war = War()
+    war.addViking(viking)
+    war.addSaxon(saxon)
+
+    print(war)
