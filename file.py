6617cadfa07bbf9443dbe632a745ddd4903299f0
danisiaj <123707285+danisiaj@users.noreply.github.com>
2024-09-13 17:22:28 +0200

    Vikings project

--- file.py
+++ file.py
@@ -1,24 +1,23 @@
-
-from vikingsClasses import Soldier, Viking, Saxon, War
+from greatWar import Soldier, Viking, Saxon, War
 import random
 
-
-soldier_names = ["albert","andres","archie","dani","david","gerard","german","graham","imanol","laura"]
+soldier_names = ["Kerem","Carlos","Tiago","Dani", "Julia","Caroline","Joao","Saul","Dusan", "Jaime"]
 great_war = War()
 
 #Create 5 Vikings
 for i in range(0,5):
     if i:
-        great_war.addViking(Viking(soldier_names[random.randint(0,9)],100,random.randint(0,100)))
+        great_war.addViking(Viking(soldier_names[random.randint(0,9)],300,random.randint(0,100)))
 
 #Create 5 Saxons
 for i in range(0,5):
     if i:
-        great_war.addSaxon(Saxon(100,random.randint(0,100)))
+        great_war.addSaxon(Saxon(300,random.randint(0,100)))
     
 round = 0
 while great_war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
     great_war.vikingAttack()
+    
     if len(great_war.saxonArmy) != 0:
         great_war.saxonAttack()
     print(f"round: {round} // Viking army: {len(great_war.vikingArmy)} warriors",f"and Saxon army: {len(great_war.saxonArmy)} warriors")
--- greatWar.py
+++ greatWar.py
@@ -53,16 +53,13 @@ class Viking(Soldier):
         self.name = name
         self.health = health
         self.strength = strength
+        self.shield = 0
 
-    def battleCry(self):
-        """With this method, Vikings call for their battlecry (no effects in this program)"""
+        self.battleCry == True # Special ability for Vikings (health +50)
+        self.shieldWall == True # Can activate the special ability (shield +50)
 
-        """POSSIBLE EXTRA:
-        health_recovery = 50 # Battle Cry restores vikings health +50 points
-        self.health += health_recovery
-        return (f"For Valhalla! Health +{health_recovery} points. Health: {self.health}")
-        """
-        return "Odin Owns You All!"
+        self.berserkerRage == False # special ability for Vikings (strength x2)
+
 
     def receiveDamage(self, damage):
         """
@@ -70,11 +67,42 @@ class Viking(Soldier):
         It inherints the method recieve_damage() from the Soldier Class
         arg1: int. Damage
         """
-        self.health -= damage # substract the amount of damage to the Viking's health and check if it's more than 0
+        self.health -= (damage - self.shield) # substract the amount of the result (damage - shield)
         if self.health > 0:
             return self.name + f" has received {damage} points of damage"
         elif self.health <= 0:
             return self.name + " has died in act of combat"       
+        
+    def battleCry(self):
+        """
+        Effect: When a Viking activates his Battlecry, his health increases 50 points. ONCE
+        """
+
+        if self.battleCry == True:
+            
+            health_recovery = 50 # Battle Cry restores vikings health +50 points
+            self.health += health_recovery
+            self.battleCry = False
+            return f"For Valhalla! Health +{health_recovery} points. Health: {self.health}"
+        else:
+            return f"{self.name} can't use Battlecry"
+
+    def berserkerRage(self):
+        """
+        Effect: when health drops bellow 30 points. Vikings enter a state of rage, 
+        where their strength doubles for one turn.
+        """
+
+        self.strength *= 2 # Strength doubles
+        return f"{self.name} has enter Berserker Rage Mode"
+    
+    def shieldWall(self):
+        """
+        Effect: when 50% of vikings have died, Shield Wall is activated
+        and viking's protective shield increases to 50 points (damage =  damage - 50)
+        """
+        self.shield += 50
+
 
 # Saxon
 
@@ -99,6 +127,14 @@ class Saxon(Soldier):
             return f"A Saxon has received {damage} points of damage"
         elif self.health <= 0:
             return "A Saxon has died in combat"    
+    
+    def arrowBarrage(self):
+        """
+        Effect: Saxons can unleash a rain of arrows hitting multiple enemies at once. activates only once.
+        """
+        barrage_damage = 50 # standard damage to all enemies 
+        return barrage_damage
+        
 
 
 # Davicente
@@ -138,29 +174,61 @@ class War():
         viking = random.choice(self.vikingArmy) # select random viking that will attack
         damage = viking.strength # store the viking's strength in damage to substract it from the saxon's health
         saxon = random.choice(self.saxonArmy) # select random saxon to be attacked
-        attack_result = saxon.receiveDamage(damage) # Saxon received damaged and health is depleted an amount of "damage"
 
+        if viking.berserkerRage == True: # check if viking has entered special ability Berserker Rage Mode (strength x2)
+            viking.berserkerRage() # Activate special ability 
+            viking.berserkerRage == False # Deactivates special ability
+        else:
+            pass
+        attack_result = saxon.receiveDamage(damage) # Saxon received damaged and health is depleted an amount of "damage"
+        
         if saxon.health <= 0: # Check if the Saxon is still alive (health > 0)
             self.saxonArmy.remove(saxon) # Saxon dies and is removed from the army
             return attack_result
         else:
             return attack_result
+        
     
     def saxonAttack(self):
         """
         Initializes attack() method from a random Saxon to a random Viking
         appying damage equal to the Saxon's strength
         """        
-        saxon = random.choice(self.saxonArmy) # select random Saxon that will attack
-        damage = saxon.strength # store the saxon's strength in damage to substract it from the vikings's health
-        viking = random.choice(self.vikingArmy) # select random viking to be attacked
-        attack_result = viking.receiveDamage(damage) # Viking received damaged and health is depleted an amount of "damage"
 
-        if viking.health <= 0: # Check if the Viking is still alive (health > 0)
-            self.vikingArmy.remove(viking) # Viking dies and is removed from the army
+        saxon = random.choice(self.saxonArmy) # select random Saxon that will attack
+
+        # Chance to activate Arrow Barrage before the atacck
+        chance_arrowBarrage = random.randint(0,5)
+        if chance_arrowBarrage == 0:
+            saxon.arrowBarrage() # return {damage = 50}
+            for viking in self.vikingArmy:
+                viking.health -= 50
 
         else:
-            return attack_result
+            damage = saxon.strength # store the saxon's strength in damage to substract it from the vikings's health
+            viking = random.choice(self.vikingArmy) # select random viking to be attacked
+
+            # chance to activate Battlecry before the attack
+            if viking.battleCry == True: # Checks ability to activate special ability Battlecry
+                chance = random.choice(1,2) # 50% chance to Activate special ability Battlecry
+                if chance == 1:
+                    return viking.battleCry() # viking's health restored +50 points
+                else:
+                    pass
+            else:
+                pass
+
+            attack_result = viking.receiveDamage(damage) # Viking received damaged and health is depleted an amount of "damage"
+
+            if viking.health <= 0: # Check if the Viking is still alive (health > 0)
+                self.vikingArmy.remove(viking) # Viking dies and is removed from the army
+
+            else:
+                if viking.health <= 30: 
+                    viking.berserkerRage == True # viking has entered Berserker Rage Mode
+                    return attack_result
+                else:
+                    return attack_result
 
     def showStatus(self):
         """
@@ -177,4 +245,14 @@ class War():
             return "Saxons have fought for their lives and survive another day..."
         
         elif len(self.saxonArmy) > 0 and len(self.vikingArmy) > 0: # BATTLE STILL ON
+            if len(self.vikingArmy) < 3:
+                for viking in self.vikingArmy:
+                    if viking.shieldWall == True:
+                        viking.shieldWall()
+                        print(f"{viking.name} has activated shield Wall")
+                        viking.shieldWall == False
+                    else:
+                        pass
+            else:
+                pass
             return "Vikings and Saxons are still in the thick of battle."
