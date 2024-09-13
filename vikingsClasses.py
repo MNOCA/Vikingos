4b22936248777e113058075ca0956b39714e466a
Dudokic <dusan.dokic.dd@gmail.com>
2024-09-13 14:31:00 +0200

    Viking project Dusan

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -6,12 +6,16 @@ import random
 class Soldier:
     def __init__(self, health, strength):
         # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
         # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
     
 
 # Viking
@@ -19,65 +23,86 @@ class Soldier:
 class Viking(Soldier):
     def __init__(self, name, health, strength):
         # your code here
+        super().__init__(health, strength)
+        self.name = name
 
     def battleCry(self):
         # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        if self.health <= 0:
+            return f"{self.name} has died in act of combat"
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
 
+
+    	
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
         # your code here
+        super().__init__(health, strength)
+
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        if self.health <= 0:
+            return "A Saxon has died in combat"
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+
+
 
 # Davicente
 
 class War():
     def __init__(self):
         # your code here
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, viking):
         # your code here
-    
+        self.vikingArmy.append(viking)
+
     def addSaxon(self, saxon):
         # your code here
-    
+        self.saxonArmy.append(saxon)
+
     def vikingAttack(self):
         # your code here
-    
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+        result = saxon.receiveDamage(viking.strength)
+        if saxon.health <= 0:
+            self.saxonArmy.remove(saxon)
+        return result
+        
     def saxonAttack(self):
         # your code here
+        if len(self.saxonArmy) > 0:
+            saxon = random.choice(self.saxonArmy)
+            viking = random.choice(self.vikingArmy)
+            result = viking.receiveDamage(saxon.strength)
+            if viking.health <= 0:
+                self.vikingArmy.remove(viking)
+            return result
+        else:
+            return "No Saxons left to attack"
+        
 
     def showStatus(self):
         # your code here
-    pass
-
-#yop
-class War2:
-
-    def __init__(self):
-        # your code here
-
-    def addViking(self, Viking):
-        # your code here
-    
-    def addSaxon(self, Saxon):
-        # your code here
-    
-    def vikingAttack(self):
-        # your code here
-
-    def saxonAttack(self):
-        # your code here
-
-    def showStatus(self):
-        # your code here
-
-    pass
-
+        if self.saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        elif self.vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+        
 
---
+++ wargame_dusan.py
@@ -0,0 +1,34 @@
+from vikingsClasses import Soldier, Viking, Saxon, War
+import random
+
+vikingNames = ["Kerem", "Julia", "Caroline", "Saul", "Joao" , "Carlos", "Dani", "Tiago"]
+war = War()
+
+# Adding the Viking
+
+for i in range(0,8):
+        viking = Viking(vikingNames[i],random.randint(1,100),random.randint(1,20))
+        war.addViking(viking)
+        print(f"{viking.name} has been added to the game with {viking.health} Health and {viking.strength} Strength")
+       
+
+          
+#Adding the saxons
+
+for i in range(1,8):
+    saxon = Saxon(random.randint(1,100),random.randint(1,20))
+    war.addSaxon(saxon)
+    print(f"Saxon Number {i} has been added to the game wirh {saxon.health} Health and {saxon.strength} Strength")
+
+#Starting the Game
+round_of_war = 0
+while war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
+    war.vikingAttack()
+    war.saxonAttack()
+    print(f"Round Number {round_of_war}. Number of remaining Vikings {len(war.vikingArmy)} vs. Number of remaining Saxons {len(war.saxonArmy)}")
+    war.showStatus()
+    round_of_war += 1
+
+#Final Status of the War
+
+print(war.showStatus())
